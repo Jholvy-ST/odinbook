0c72cfe794131c3f4e5df33e75df836b4dae1e10
Jholvy Sarmiento Torres <jholvysarmiento@gmail.com>
2022-05-19 18:59:17 -0500

    Changed facebook function

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ passport.use(new facebookStrategy({
 	passReqToCallback: true
 
 },// facebook will send back the token and profile
-function(token, refreshToken, profile, cb) {
+/*function(token, refreshToken, profile, cb) {
 
 	// asynchronous
 	process.nextTick(function() {
@@ -91,7 +91,7 @@ function(token, refreshToken, profile, cb) {
 
 							// set all of the facebook information in our user model
 							newUser.uid    = profile.id; // set the users facebook id                  
-							//newUser.token = token; // we will save the token that facebook provides to the user                    
+							newUser.token = token; // we will save the token that facebook provides to the user                    
 							newUser.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
 							newUser.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
 							newUser.gender = profile.gender
@@ -110,7 +110,13 @@ function(token, refreshToken, profile, cb) {
 
 	})
 
-}));
+}*/
+	function(accessToken, refreshToken, profile, cb) {
+		User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+			return cb(err, user);
+		});
+	}
+));
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);

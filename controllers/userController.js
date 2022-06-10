d212fa3da3f323c58cce8ded6d84d89db4e99e95
Jholvy Sarmiento Torres <jholvysarmiento@gmail.com>
2022-06-10 13:23:26 -0500

    Returning post in comment_post

--- controllers/userController.js
+++ controllers/userController.js
@@ -257,7 +257,7 @@ exports.comment_post = [
 
 			Post.findByIdAndUpdate(req.body.id, post, {}, function (err) {
 				if (err) { return next(err); }
-				res.send({message: 'Done'})
+				res.send({post: found_post})
 			});
 
 		})

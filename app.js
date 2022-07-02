e7f583c569023da4c3248fa878a54146da15a0f6
Jholvy Sarmiento Torres <jholvysarmiento@gmail.com>
2022-07-02 12:33:45 -0500

    Added test route

--- app.js
+++ app.js
@@ -192,7 +192,7 @@ app.use(helmet());
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());

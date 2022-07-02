16d2752dc0a2660228b50f49e41745853a938519
Jholvy Sarmiento Torres <jholvysarmiento@gmail.com>
2022-07-02 12:19:27 -0500

    Added test route

--- app.js
+++ app.js
@@ -231,8 +231,13 @@ app.get('/auth/facebook/token', passport.authenticate('facebook-token', { sessio
 	//res.send({message: 'Done'})
 });
 
-app.post('/auth/local', passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+app.post('/auth/local', passport.authenticate('local', { session: false }), (req, res) => {
 	return res.json(req.user);
+	//res.send({message: 'Done'})
+});
+
+app.get('/test', (req, res) => {
+	return res.json({data: 'Response'});
 	//res.send({message: 'Done'})
 });
 
